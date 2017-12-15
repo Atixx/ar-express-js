74111b10077bd95a18a4f7916f95bd572ef97a38
Alan Rinaldi <alan.rinaldi@wolox.com.ar>
2017-12-15 17:56:21 -0300

    Sign Up Test

--- test/users.js
+++ test/users.js
@@ -1,5 +1,6 @@
 const chai = require('chai'),
   server = require('./../app'),
+  dictum = require('dictum.js'),
   sessionManager = require('./../app/services/sessionManager'),
   should = chai.should();
 
@@ -9,7 +10,127 @@ describe('users', () => {
       chai
         .request(server)
         .post('/users/sessions')
-        .send({ email: 'invalid', password: '1234' })
+        .send({ email: 'invalid', password: '12345678' })
+        .catch(err => {
+          err.should.have.status(400);
+          err.response.should.be.json;
+          err.response.body.should.have.property('error');
+        })
+        .then(() => done());
+    });
+  });
+
+  describe('users create', () => {
+    it('creation OK', done => {
+      chai
+        .request(server)
+        .post('/users')
+        .send({
+          firstname: 'Alan',
+          lastname: 'Rinaldi',
+          email: 'alan.rinaldi@wolox.com.ar',
+          password: '12345678'
+        })
+        .then(res => {
+          res.should.have.status(201);
+          dictum.chai(res);
+        })
+        .then(() => done());
+    });
+    it('should fail because name is missing', done => {
+      chai
+        .request(server)
+        .post('/users')
+        .send({
+          lastname: 'Rinaldi',
+          email: 'alan.rinaldi@wolox.com.ar',
+          password: '1234'
+        })
+        .catch(err => {
+          err.should.have.status(400);
+          err.response.should.be.json;
+          err.response.body.should.have.property('error');
+        })
+        .then(() => done());
+    });
+    it("should fail because password isn't alphanumeric", done => {
+      chai
+        .request(server)
+        .post('/users')
+        .send({
+          firstname: 'Alan',
+          lastname: 'Rinaldi',
+          email: 'alan.rinaldi@wolox.com.ar',
+          password: '123??56234'
+        })
+        .catch(err => {
+          err.should.have.status(400);
+          err.response.should.be.json;
+          err.response.body.should.have.property('error');
+        })
+        .then(() => done());
+    });
+    it('should fail because password is short', done => {
+      chai
+        .request(server)
+        .post('/users')
+        .send({
+          firstname: 'Alan',
+          lastname: 'Rinaldi',
+          email: 'alan.rinaldi@wolox.com.ar',
+          password: '1234'
+        })
+        .catch(err => {
+          err.should.have.status(400);
+          err.response.should.be.json;
+          err.response.body.should.have.property('error');
+        })
+        .then(() => done());
+    });
+    it('should fail because of invalid email', done => {
+      chai
+        .request(server)
+        .post('/users')
+        .send({
+          firstname: 'Alan',
+          lastname: 'Rinaldi',
+          email: 'alan.rinaldi',
+          password: '12345678'
+        })
+        .catch(err => {
+          err.should.have.status(400);
+          err.response.should.be.json;
+          err.response.body.should.have.property('error');
+        })
+        .then(() => done());
+    });
+    it("should fail because email isn't woloxmail", done => {
+      chai
+        .request(server)
+        .post('/users')
+        .send({
+          firstname: 'Alan',
+          lastname: 'Rinaldi',
+          email: 'alan.rinaldi@gmail.com.ar',
+          password: '12345678'
+        })
+        .catch(err => {
+          err.should.have.status(400);
+          err.response.should.be.json;
+          err.response.body.should.have.property('error');
+        })
+        .then(() => done());
+    });
+    it('should fail because email must be unique', done => {
+      chai
+        .request(server)
+        .post('/users')
+        .send({
+          firstname: 'Alan',
+          lastname: 'Rinaldi',
+          email: 'alan.rinaldi@wolox.com.ar',
+          password: '12345678'
+        })
         .catch(err => {
           err.should.have.status(400);
           err.response.should.be.json;
