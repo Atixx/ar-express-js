235d9447865efa62e541e595662675a6d1e3b936
Alan Rinaldi <alan.rinaldi@wolox.com.ar>
2017-12-26 14:02:47 -0300

    Environment variables fixed

--- config/index.js
+++ config/index.js
@@ -28,7 +28,12 @@ const assignObject = (target, source) => {
 const config = {
   common: {
     database: {
-      url: 'postgres://postgres@localhost:5432/woloxland'
+      url: process.env.NODE_API_DB_URL,
+      host: process.env.NODE_API_DB_HOST,
+      port: process.env.NODE_API_DB_PORT,
+      name: process.env.NODE_API_DB_NAME,
+      username: process.env.NODE_API_DB_USERNAME,
+      password: process.env.NODE_API_DB_PASSWORD
     },
     api: {
       bodySizeLimit: process.env.API_BODY_SIZE_LIMIT,
