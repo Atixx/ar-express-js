4d798ccfac3f3c6701db488e077ddc7e931029cd
Alan Rinaldi <alan.rinaldi@wolox.com.ar>
2017-12-26 12:13:42 -0300

    logout all test

--- test/sessions.js
+++ test/sessions.js
@@ -60,6 +60,9 @@ describe('/users/sessions POST', () => {
         .then(() => done());
     });
   });
+});
+
+describe('/users/logout POST', () => {
   it('Should successful because user logout correctly', done => {
     return successfulLogin().then(res => {
       res.should.have.status(201);
@@ -83,6 +86,88 @@ describe('/users/sessions POST', () => {
             })
             .then(() => done());
         });
+    });
+  });
+  it('Should fail because of invalid token', done => {
+    return successfulLogin().then(res => {
+      res.should.have.status(201);
+      res.should.be.json;
+      res.headers.should.have.property(sessionManager.HEADER_NAME);
+      return sessionService.getCount(res.body.email).then(count => {
+        count.should.to.equal(1);
+        delay(1000).then(() => {
+          return chai
+            .request(server)
+            .post('/users/logout')
+            .send({ email: res.body.email })
+            .set(sessionManager.HEADER_NAME, res.headers[sessionManager.HEADER_NAME])
+            .catch(err => {
+              return sessionService.getCount(res.body.email).then(count2 => {
+                count2.should.to.equal(0);
+              });
+            })
+            .then(() => done());
+        });
+      });
+    });
+  });
+});
+
+describe('/users/logout/all POST', () => {
+  it('Should fail because of invalid token', done => {
+    return successfulLogin().then(res => {
+      res.should.have.status(201);
+      res.should.be.json;
+      res.headers.should.have.property(sessionManager.HEADER_NAME);
+      delay(1000).then(() => {
+        return successfulLogin().then(res2 => {
+          res2.should.have.status(201);
+          res2.should.be.json;
+          res2.headers.should.have.property(sessionManager.HEADER_NAME);
+          return sessionService.getCount(res2.body.email).then(count => {
+            count.should.to.equal(2);
+            return chai
+              .request(server)
+              .post('/users/logout/all')
+              .send({ email: res.body.email })
+              .set(sessionManager.HEADER_NAME, res.headers[sessionManager.HEADER_NAME])
+              .catch(err => {
+                return sessionService.getCount(res.body.email).then(count2 => {
+                  count2.should.to.equal(1);
+                });
+              })
+              .then(() => done());
+          });
+        });
+      });
+    });
+  });
+  it('Should successful because sessions database is empty', done => {
+    return successfulLogin().then(res => {
+      res.should.have.status(201);
+      res.should.be.json;
+      res.headers.should.have.property(sessionManager.HEADER_NAME);
+      delay(200).then(() => {
+        return successfulLogin().then(res2 => {
+          res2.should.have.status(201);
+          res2.should.be.json;
+          res2.headers.should.have.property(sessionManager.HEADER_NAME);
+          return sessionService.getCount(res2.body.email).then(count => {
+            count.should.to.equal(2);
+            return chai
+              .request(server)
+              .post('/users/logout/all')
+              .send({ email: res.body.email })
+              .set(sessionManager.HEADER_NAME, res.headers[sessionManager.HEADER_NAME])
+              .then(res3 => {
+                return sessionService.getCount(res.body.email).then(count2 => {
+                  count2.should.to.equal(0);
+                });
+              })
+              .then(() => done());
+          });
+        });
+      });
     });
   });
 });
