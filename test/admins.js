dbe541de41805d3b8127d82bbbb2dfffbfca9a88
Alan Rinaldi <alan.rinaldi@wolox.com.ar>
2017-12-27 17:20:02 -0300

    admin test

---
+++ test/admins.js
@@ -0,0 +1,16 @@
+const chai = require('chai'),
+  server = require('./../app'),
+  dictum = require('dictum.js'),
+  sessionManager = require('./../app/services/sessionManager'),
+  should = chai.should();
+
+describe('Admin test', () => {
+  describe('/admin/users POST', () => {
+    it('Should be successful', done => {
+
+    });
+    it('Should fail because of invalid user', done => {
+      
+    });
+  });
+});
