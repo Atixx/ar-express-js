e2cccdc61550643cc8e3ff04fb5b9e7e9ef57825
Alan Rinaldi <alan.rinaldi@wolox.com.ar>
2017-12-28 14:17:52 -0300

    Test Fixed

---
+++ test/games.js
@@ -0,0 +1,81 @@
+const chai = require('chai'),
+  server = require('./../app'),
+  dictum = require('dictum.js'),
+  sessionManager = require('./../app/services/sessionManager'),
+  orm = require('./../app/orm'),
+  errors = require('./../app/errors'),
+  should = chai.should();
+
+const successfulLogin = cb => {
+  return chai
+    .request(server)
+    .post('/users/sessions')
+    .send({ email: 'email1@wolox.com.ar', password: '12345678' });
+};
+
+describe('/games POST', () => {
+  it('should be successful', done => {
+    return successfulLogin().then(res => {
+      return chai
+        .request(server)
+        .post('/games')
+        .set(sessionManager.HEADER_NAME, res.headers[sessionManager.HEADER_NAME])
+        .send({
+          email: 'email1@wolox.com.ar',
+          game: {
+            name: 'game2',
+            code: 'code2',
+            score: 10
+          }
+        })
+        .then(res2 => {
+          res2.should.have.status(201);
+          dictum.chai(res2);
+        })
+        .then(() => done());
+    });
+  });
+  it('should fail because name is missing', done => {
+    return successfulLogin().then(res => {
+      return chai
+        .request(server)
+        .post('/games')
+        .set(sessionManager.HEADER_NAME, res.headers[sessionManager.HEADER_NAME])
+        .send({
+          email: 'email1@wolox.com.ar',
+          game: {
+            code: 'code2',
+            score: 10
+          }
+        })
+        .catch(err => {
+          err.should.have.status(400);
+          err.response.should.be.json;
+          err.response.text.should.include('name cannot be null');
+        })
+        .then(() => done());
+    });
+  });
+  it('should fail because code must be unique', done => {
+    return successfulLogin().then(res => {
+      return chai
+        .request(server)
+        .post('/games')
+        .set(sessionManager.HEADER_NAME, res.headers[sessionManager.HEADER_NAME])
+        .send({
+          email: 'email1@wolox.com.ar',
+          game: {
+            name: 'game2',
+            code: 'code1',
+            score: 10
+          }
+        })
+        .catch(err => {
+          err.should.have.status(400);
+          err.response.should.be.json;
+          err.response.text.should.include('code must be unique');
+        })
+        .then(() => done());
+    });
+  });
+});
