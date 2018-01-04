21379ef5ce41528bc938989d9b4c50b3a0f14fb9
Alan Rinaldi <alan.rinaldi@wolox.com.ar>
2018-01-04 16:55:19 -0300

    index fixed

--- config/index.js
+++ config/index.js
@@ -30,7 +30,7 @@ const config = {
     database: {
       url: process.env.NODE_API_DB_URL,
       host: process.env.NODE_API_DB_HOST,
-      port: process.env.PORT,
+      port: process.env.NODE_API_DB_PORT,
       name: process.env.NODE_API_DB_NAME,
       username: process.env.NODE_API_DB_USERNAME,
       password: process.env.NODE_API_DB_PASSWORD
